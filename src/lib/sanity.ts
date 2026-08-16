75f67f565d60b76e368dddfd208708d58e1ac6d3
Dip0375 <dipnarayan.cse@gmail.com>
2026-08-16 23:01:36 +0530

    chore: move Sanity credentials to env variables

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -2,11 +2,11 @@ import { createClient } from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
 export const sanityClient = createClient({
-  projectId: 'cq69q11x',
-  dataset: 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   apiVersion: '2024-05-22',
   useCdn: true,
-  token: 'skxp7IoocunogEXQBsVa46yC1MzJDUBOWUHTxzbZOoClpyHZLf6xupOg1eMjVSB3vor1fWjAwBFi2kE8P5Oa7K1bcl1DU3iG6I1RVSApjouRw3YTPHTTF4AzcH2MxCyD7vn9D42CMOGRLqv3kSnHVjIpFDmMEkXvpqeoAq2n4wRcoyiAybis'
+  token: process.env.SANITY_API_TOKEN,
 })
 
 
